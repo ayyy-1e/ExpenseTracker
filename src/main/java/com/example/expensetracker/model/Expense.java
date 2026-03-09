e85036d3dfc22bdb839c9de30c86974695590082
eyyy.1s <emisopheak@gmail.com>
2026-03-09 21:38:45 +0700

    git add. git commit -m "PostgreSQL database setup2" git push

--- src/main/java/com/example/expensetracker/model/Expense.java
+++ src/main/java/com/example/expensetracker/model/Expense.java
@@ -1,6 +1,13 @@
 package com.example.expensetracker.model;
 
+import jakarta.persistence.*;
+
+@Entity
 public class Expense {
+
+    @Id
+    @GeneratedValue(strategy = GenerationType.IDENTITY)
+    private Long id;
 
     private String name;
     private double amount;
@@ -12,16 +19,24 @@ public class Expense {
         this.amount = amount;
     }
 
+    public Long getId() {
+        return id;
+    }
+
     public String getName() {
         return name;
     }
 
-    public void setName(String name) {
-        this.name = name;
-    }
-
     public double getAmount() {
         return amount;
+    }
+
+    public void setId(Long id) {
+        this.id = id;
+    }
+
+    public void setName(String name) {
+        this.name = name;
     }
 
     public void setAmount(double amount) {
